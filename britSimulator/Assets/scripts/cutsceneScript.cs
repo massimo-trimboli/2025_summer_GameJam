82606826baf0d84e6b72eff8978ca5558df6ebc9
massimo <masst123543@gmail.com>
2025-07-12 18:55:57 -0400

    cutscene scene complete

--- britSimulator/Assets/scripts/cutsceneScript.cs
+++ britSimulator/Assets/scripts/cutsceneScript.cs
@@ -6,21 +6,36 @@ using UnityEngine.Video;
 public class cutsceneScript : MonoBehaviour
 {
     public GameObject scriptObject;
+
+    public GameObject button1;
+    public GameObject skipButton;
+
     // Start is called before the first frame update
     void Start()
     {
         VideoPlayer player = GetComponent<VideoPlayer>();
         player.loopPointReached += nextScene;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        button1.SetActive(true);
+        skipButton.SetActive(false);
     }
 
     void nextScene(VideoPlayer player)
     {
         scriptObject.GetComponent<menuScript>().goDoTutorial();
+    }
+
+    public void summonButton()
+    {
+        button1.SetActive(false);
+        skipButton.SetActive(true);
+
+        Invoke("unSummonButton", 3f);
+    }
+
+    void unSummonButton()
+    {
+        button1.SetActive(true);
+        skipButton.SetActive(false);
     }
 }
